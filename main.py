d2cd6636dfd1506b311c067f224929d1e27e00d7
LorenzoIlMagnifico <49073691+LorenzoIlMagnifico@users.noreply.github.com>
2021-11-25 19:28:51 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ def main():
         print("Please provide an input file, a value for alpha and a time limit in seconds! For now take 0011.txt and alpha=0.2 for 15 seconds.")
         input = "0011.txt"
         alpha = 0.2
-        limit = 15
+        limit = 5
         exit()
 
     else:
@@ -30,39 +30,99 @@ def main():
     start1 = time.process_time()
     s1 = task1(graph)
     end1 = time.process_time()
-    
+
     start2 = time.process_time()
-    s2, time2 = task2(graph, alpha, limit)
+    s2, time2 = task2(graph, alpha, limit, method="mst")
     end2 = time.process_time()
+
+    start3 = time.process_time()
+    s3, time3 = task2(graph, alpha, limit, method="path")
+    end3 = time.process_time()
     
     print(f"Running time for task1 was: {end1 - start1}")
     print(f"Best solution was {s1.evaluate()}")
-    print(f"Running time for task2 was: {end2 - start2}")
+    print(f"Running time for task2 mst was: {end2 - start2}")
     print(f"Best solution was {s2.evaluate()}")
+    print(f"Running time for task2 path was: {end3 - start3}")
+    print(f"Best solution was {s3.evaluate()}")
     
     output1 = input.replace(".txt", "_vnd.txt")
-    output2 = input.replace(".txt", "_grasp.txt")
+    output2 = input.replace(".txt", "_mst_grasp.txt")
+    output3 = input.replace(".txt", "_path_grasp.txt")
     outputlog = input.replace(".txt", "_log.txt")
 
     s1.write_solution_to_file(graph, output1)
     s2.write_solution_to_file(graph, output2)
+    s3.write_solution_to_file(graph, output3)
     
     f = open(outputlog, "w")
     f.write(f"Log for instance {input}.\n\n")
     
     f.write(f"VND used alpha=0 and took {end1-start1} seconds.\n")
     f.write(f"{s1}\n\n")
-    
-    f.write(f"GRASP used alpha={alpha} and took {end2-start2} seconds. The best instance was found after {time2} seconds.\n")
-    f.write(f"{s2}")
-    
 
-def random_path(g: Graph, source, target, weight, cutoff):
-    itr = nx.all_simple_paths(g.graph, source, target, cutoff)#cutoff is the search depth not the weight of the graph
-    path = next(itr)
-    length = nx.classes.path_weight(g.graph, path, weight)
-    return path, length
+    f.write(f"GRASP with mst used alpha={alpha} and took {end2-start2} seconds. The best instance was found after {time2} seconds.\n")
+    f.write(f"{s2}\n\n")
 
+    f.write(f"GRASP with path used alpha={alpha} and took {end3 - start3} seconds. The best instance was found after {time3} seconds.\n")
+    f.write(f"{s3}")
+    
+def runall_with_grid(alpha_grid = [0.05,0.1,0.2,0.4,0.7,0.95]):
+    files = ["0125.txt", "1331.txt"]
+    f = open("girdsearchlog", "w")
+
+    for file in files:
+        best_method ,best_alpha, best_s = run_with_grid(file, 600, alpha_grid)
+        f.write(f"Log for instance {file}.\n\n")
+        f.write(f"best method : {best_method}\n")
+        f.write(f"best alpha : {best_alpha}\n")
+        f.write(f"best solution with val {best_s.evaluate()} \n {best_s}\n\n")
+
+def run_with_grid(input, limit, alpha_grid):
+    if input == "0011.txt":
+        limit = 1
+    print(f"Loading input graph from {input}...")
+    graph = Graph()
+    graph.load_graph_from_file(input)
+
+    best_s = None
+    best_alpha = None
+    best_method = None
+
+    for alpha in alpha_grid:
+        start2 = time.process_time()
+        s2, time2 = task2(graph, alpha, limit, method="mst")
+        end2 = time.process_time()
+
+        start3 = time.process_time()
+        s3, time3 = task2(graph, alpha, limit, method="path")
+        end3 = time.process_time()
+        print(f"With alpha = {alpha}")
+        print(f"Running time for task2 mst was: {end2 - start2}")
+        print(f"Best solution was {s2.evaluate()}")
+        print(f"Running time for task2 path was: {end3 - start3}")
+        print(f"Best solution was {s3.evaluate()}")
+        if best_s is None:
+            if s2.evaluate() < s3.evaluate():
+                best_s = s2
+                best_alpha = alpha
+                best_method = "mst"
+            else:
+                best_s = s3
+                best_alpha = alpha
+                best_method = "path"
+        else:
+            if s2.evaluate() < best_s.evaluate():
+                best_s = s2
+                best_alpha = alpha
+                best_method = "mst"
+            elif s3.evaluate() < best_s.evaluate():
+                best_s = s3
+                best_alpha = alpha
+                best_method = "path"
+    output = input.replace(".txt", f"best_grasp.txt")
+    best_s.write_solution_to_file(graph, output)
+    return best_method, best_alpha, best_s
 
 def randomized_mst(dg : nx.Graph, alpha : float):
     key = lambda e: dg.get_edge_data(*e)["weight"]
@@ -104,9 +164,31 @@ def randomized_steiner_tree(g : Graph, tree : int, alpha : float):
     dg = distance_graph(g, getattr(g, f"terminal_{tree}"), f"weight_{tree}")
     mst = randomized_mst(dg, alpha)
 
+
+    terminal = getattr(g, f"terminal_{tree}")
+    for t in terminal:
+        if not t in mst.nodes:
+            print("\n\n\nnot in terminal\n\n\n")
+
+
     edges, weight, terminals = rebuild_steiner_tree(g, mst, tree)
     return edges, weight, terminals
 
+
+def random_path(g: Graph, source, target, weight, cutoff):
+    itr = nx.all_simple_paths(g.graph, source, target, cutoff)#cutoff is the search depth not the weight of the graph
+    path = next(itr)
+    ctr = 0
+    path_lst = []
+    while True:
+        ctr += 1
+        path_lst.append(path)
+        path = next(itr, None)
+        if path is None or ctr > 3000:
+            break
+    path = random.choice(path_lst)
+    length = nx.classes.path_weight(g.graph, path, weight)
+    return path, length
 
 def randomized_greedy_steiner_tree(g: Graph, weight, terminal_nodes, alpha = 0.5):#alpha gives the factor how much more nodes the choosen path can have than the shortest path
     pair_shortest_paths = {}
@@ -150,27 +232,32 @@ def randomized_greedy_steiner_tree(g: Graph, weight, terminal_nodes, alpha = 0.5
     return steiner_tree
 
 
-def construction_heuristic(g : Graph, alpha = 0):
+def construction_heuristic(g : Graph, alpha = 0, method = "mst"):
     s = Solution()
+    if method == "path":
+        st_1 = randomized_greedy_steiner_tree(g, "weight_1", g.terminal_1, alpha = alpha)
+        st_2 = randomized_greedy_steiner_tree(g, "weight_2", g.terminal_2, alpha = alpha)
 
-    #st_1 = randomized_greedy_steiner_tree(g, "weight_1", g.terminal_1, alpha = alpha)
-    #st_2 = randomized_greedy_steiner_tree(g, "weight_2", g.terminal_2, alpha = alpha)
+        s.key_nodes_1 = [n for n in st_1.nodes if st_1.degree(n) >= 3 and n not in g.terminal_1]
+        s.key_nodes_2 = [n for n in st_2.nodes if st_2.degree(n) >= 3 and n not in g.terminal_2]
+
+        s.edges_1 = set(st_1.edges)
+        s.edges_2 = set(st_2.edges)
+
+        s.weight_1 = st_1.size("weight_1")
+        s.weight_2 = st_2.size("weight_2")
+
+        compute_shared_edges(g, s)
+
+        return s
+
+    if method == "mst":
+        s.edges_1, s.weight_1, s.key_nodes_1 = randomized_steiner_tree(g, 1, alpha)
+        s.edges_2, s.weight_2, s.key_nodes_2 = randomized_steiner_tree(g, 2, alpha)
     
-    #s.key_nodes_1 = [n for n in st_1.nodes if st_1.degree(n) >= 3 and n not in g.terminal_1]
-    #s.key_nodes_2 = [n for n in st_2.nodes if st_2.degree(n) >= 3 and n not in g.terminal_2]
-    
-    #s.edges_1 = set(st_1.edges)
-    #s.edges_2 = set(st_2.edges)
-    
-    #s.weight_1 = st_1.size("weight_1")
-    #s.weight_2 = st_2.size("weight_2")
-    
-    s.edges_1, s.weight_1, s.key_nodes_1 = randomized_steiner_tree(g, 1, alpha)
-    s.edges_2, s.weight_2, s.key_nodes_2 = randomized_steiner_tree(g, 2, alpha)
-    
-    compute_shared_edges(g, s)
-    
-    return s
+        compute_shared_edges(g, s)
+
+        return s
 
 
 def distance_graph(g : Graph, ns : [int], weight : str):
@@ -213,32 +300,66 @@ def rebuild_steiner_tree(g : Graph, dg : nx.Graph, tree : int):
         mst.remove_nodes_from(to_remove)
     
     # translate distance graph edges to original graph edges
+    edges_weighted = set()
     for (n1,n2) in mst.edges():
         last_n = None
         for n in dg.get_edge_data(n1,n2)["path"]:
             if last_n is None:
                 last_n = n
                 continue
+            w = g.graph.get_edge_data(last_n, n)
+            if f"weight_{tree}_mod" in w:
+                w = w[f"weight_{tree}_mod"]
+            else:
+                w = w[f"weight_{tree}"]
             edges.add(utils.ordered_edge(last_n, n))
+            edges_weighted.add(utils.ordered_edge_weighted(last_n, n, w))
             last_n = n
+
+
+    #   For cycle detection build the graph and build the mst unfortunately this is the fastest I could think of for cycle detection.
+    #   Because if we use the cycle function of nx we still need to figure out which path is the best to delete which seems like a very daunting task
+    #   Therefore cycle deletion is just build the mst and then remove nodes of degree 1 that arent terminal nodes.
+    nodes = set()
+    graph = nx.Graph()
+    for n1,n2 in edges:
+        nodes.add(n1)
+        nodes.add(n2)
+    graph.add_nodes_from(nodes)
+    graph.add_weighted_edges_from(edges_weighted)
+    #to remove cycles build the mst of the supposed steiner tree
+    st_mst = nx.minimum_spanning_tree(graph,"weight")
+    if st_mst.edges != graph.edges:
+        #if the edges changed at least one cycle was removed therefore we need to trim the tree further
+        #we do the trimming as before
+        changed = True
+        while changed:
+            changed = False
+            to_remove = []
+
+            for n in st_mst.nodes:
+                if n not in terminals and st_mst.degree(n) == 1:
+                    to_remove.append(n)
+                    changed = True
+
+            st_mst.remove_nodes_from(to_remove)
+
 
     # We could have introduced key-nodes which are not in the MST! Therefore, we have to recompute the key-nodes!
     # Also compute the weight of the new tree.
     node_degree = {}
     total_weight = 0
-    for n1,n2 in edges:
+    for n1, n2 in st_mst.edges:
         if n1 not in node_degree:
             node_degree[n1] = 0
         if n2 not in node_degree:
             node_degree[n2] = 0
         node_degree[n1] += 1
         node_degree[n2] += 1
-        
+
         total_weight += g.graph.get_edge_data(n1, n2)[f"weight_{tree}"]
-    
-    # TODO: nodes could have degree 1 so we need another trim step :/
-    # TODO: solution could have cycles... we have to elminiate them
-    
+
+
     return edges, total_weight, [n for n, d in node_degree.items() if d >= 3 and n not in terminals]
 
 
@@ -411,7 +532,7 @@ def vnd(g : Graph, s : Solution, v : int, verbose = 0):
     return current
 
    
-def grasp(g : Graph, alpha = 0.5, max_running_time = 15, verbose = 0):
+def grasp(g : Graph, alpha = 0.5, max_running_time = 15, method = "mst", verbose = 0):
     best = None
     best_value = None
     best_time = None
@@ -419,7 +540,7 @@ def grasp(g : Graph, alpha = 0.5, max_running_time = 15, verbose = 0):
     start = time.process_time()
     running_time = 0
     while running_time < max_running_time:
-        current = construction_heuristic(g, if best is None 0 else alpha)
+        current = construction_heuristic(g, 0 if best is None else alpha, method = method)
         value = current.evaluate()
         
         if verbose == 2:
@@ -444,10 +565,10 @@ def task1(g : Graph):
     return vnd(g, init, init_value, verbose = 0)
 
 
-def task2(g : Graph, alpha : float, limit : int):
-    random.seed(123)
-    return grasp(g, alpha, limit, verbose = 0)
+def task2(g : Graph, alpha : float, limit : int, method : str):
+    return grasp(g, alpha, limit, method = method, verbose = 0)
 
 
 if __name__ == "__main__":
     main()
+    #runall_with_grid()
