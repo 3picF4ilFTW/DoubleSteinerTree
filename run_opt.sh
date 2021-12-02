2885e4bbc200c36ce7c87bc1dd53bac113f25ed8
Clemens Pircher <clemens.lukas@gmx.at>
2021-12-02 19:17:06 +0100

    optimization results

--- run_opt.sh
+++ run_opt.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
-for alpha in 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0
+for alpha_off in -75 -50 -25 25 50 75
 do
-	echo "running all instances with alpha=${alpha}"
-	for f in *.txt
-	do
-		python main.py $f $alpha 900 > /dev/null &
-	done
+	echo "running instances with alpha_off=${alpha_off}"
+	alpha=$((700 + alpha_off))
+	python main.py 0049.txt 0.$alpha 900 > /dev/null &
+	alpha=$((600 + alpha_off))
+	python main.py 0125.txt 0.$alpha 900 > /dev/null &
+	alpha=$((600 + alpha_off))
+	python main.py 1331.txt 0.$alpha 900 > /dev/null &
 	wait
-	echo "moving files into results_${alpha}"
-	mkdir results_$alpha
-	mv *_vnd.txt results_$alpha
-	mv *_grasp.txt results_$alpha
-	mv *_log.txt results_$alpha
+	echo "moving files into results_${alpha_off}"
+	mkdir results_$alpha_off
+	mv *_vnd.txt results_$alpha_off
+	mv *_grasp.txt results_$alpha_off
+	mv *_log.txt results_$alpha_off
 done
